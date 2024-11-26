64a22214a5dab12cb3922b0a69c9f5b8a88b521d
Timo Betcke <timo.betcke@gmail.com>
2024-11-26 09:38:36 +0000

    More cleanup

--- example_project/src/lib.rs
+++ example_project/src/lib.rs
@@ -2,7 +2,6 @@ use std::fmt::Display;
 
 pub use c_api_tools::cfuncs;
 pub use c_api_tools::concretise_types;
-pub use c_api_tools::eval_with_concrete_type;
 
 #[cfuncs(name = "my_wrapper", create, free, unwrap)]
 pub struct MyWrapper;
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ mod types;
 
 pub use c_api_tools_macros::cfuncs;
 pub use c_api_tools_macros::concretise_types;
-pub use c_api_tools_macros::eval_with_concrete_type;
 
 pub use types::DType;
 pub use types::DTypeIdentifier;
